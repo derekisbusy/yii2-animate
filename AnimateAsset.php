fccea8973c27f44db594dc2d6415c15f998a18ca
Derek Gifford <derekisbusy@gmail.com>
2019-06-21 21:10:41 -0400

    Updates

---
+++ AnimateAsset.php
@@ -0,0 +1,16 @@
+<?php
+namespace derekisbusy\animate;
+
+use yii\web\AssetBundle;
+
+class AnimateAsset extends AssetBundle
+{
+    public $sourcePath = '@vendor/derekisbusy/animate';
+    
+    public $css = [
+        'animate.min.css',
+    ];
+    
+    public $depends = [
+    ];
+}
